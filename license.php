1e5eb6d1f232faa9682feb78ff0570dc0e71c8cc
thmufl <thmufl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-07 15:05:38 +0000

    Fixed error when loading json data from an url
    git-svn-id: https://plugins.svn.wordpress.org/ipu-chart/trunk@853289 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- license.php
+++ license.php
@@ -2,7 +2,7 @@
 /*
 	ip4 - Templates for d3 and more.
 
-	Copyright (C) 2013  Thomas Müller Flury <thmf@ipublia.com>
+	Copyright (C) 2014  Thomas Müller Flury <thmf@ipublia.com>
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
